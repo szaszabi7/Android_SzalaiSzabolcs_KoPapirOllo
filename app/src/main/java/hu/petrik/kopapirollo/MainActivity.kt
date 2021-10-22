105d0db6a93c87876c07ada3fbf62309a4cba6b1
Szalai Szabolcs <sz.szalai16@gmail.com>
2021-10-22 23:45:53 +0200

    kész

--- app/src/main/java/hu/petrik/kopapirollo/MainActivity.kt
+++ app/src/main/java/hu/petrik/kopapirollo/MainActivity.kt
@@ -13,6 +13,14 @@ import java.util.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var gepHp1 : ImageView
+    lateinit var gepHp2 : ImageView
+    lateinit var gepHp3 : ImageView
+    lateinit var gepHpArray : Array<ImageView>
+    lateinit var jatekosHp1 : ImageView
+    lateinit var jatekosHp2 : ImageView
+    lateinit var jatekosHp3 : ImageView
+    lateinit var jatekosHpArray : Array<ImageView>
     private lateinit var imageViewEmber : ImageView
     private lateinit var imageViewGep : ImageView
     private lateinit var textEmberEredmeny : TextView
@@ -24,14 +32,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var random : Random
 
     private var gepRandom : Int = 0
-    private var win : Int = 0
-    private var lose : Int = 0
+    /*private var win : Int = 0
+    private var lose : Int = 0*/
     private var draw : Int = 0
+    private var gepHp : Int = 3
+    private var jatekosHp : Int = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        init();
+        init()
 
         gombKo.setOnClickListener() {
             imageViewEmber.setImageResource(R.drawable.rock)
@@ -43,14 +53,18 @@ class MainActivity : AppCompatActivity() {
                 textViewDontetlen.setText("Döntetlek száma: ${draw}")
             } else if (gepRandom == 1) {
                 imageViewGep.setImageResource(R.drawable.paper)
-                lose++
-                textGepEredmeny.setText("Gép: ${lose}")
+                /*lose++
+                textGepEredmeny.setText("Gép: ${lose}")*/
+                jatekosHp--
+                jatekosHpArray[jatekosHp].setImageResource(R.drawable.heart1)
                 Toast.makeText(this, "Vesztettél", Toast.LENGTH_SHORT).show()
                 gameEnd()
             } else {
                 imageViewGep.setImageResource(R.drawable.scissors)
-                win++
-                textEmberEredmeny.setText("Ember: ${win}")
+                /*win++
+                textEmberEredmeny.setText("Ember: ${win}")*/
+                gepHp--
+                gepHpArray[gepHp].setImageResource(R.drawable.heart1)
                 Toast.makeText(this, "Nyertél", Toast.LENGTH_SHORT).show()
                 gameEnd()
             }
@@ -61,9 +75,11 @@ class MainActivity : AppCompatActivity() {
             gepRandom = random.nextInt(3)
             if (gepRandom == 0) {
                 imageViewGep.setImageResource(R.drawable.rock)
-                win++
+                /*win++
+                textEmberEredmeny.setText("Ember: ${win}")*/
+                gepHp--
+                gepHpArray[gepHp].setImageResource(R.drawable.heart1)
                 Toast.makeText(this, "Nyertél", Toast.LENGTH_SHORT).show()
-                textEmberEredmeny.setText("Ember: ${win}")
                 gameEnd()
             } else if (gepRandom == 1) {
                 imageViewGep.setImageResource(R.drawable.paper)
@@ -72,9 +88,11 @@ class MainActivity : AppCompatActivity() {
                 textViewDontetlen.setText("Döntetlek száma: ${draw}")
             } else {
                 imageViewGep.setImageResource(R.drawable.scissors)
-                lose++
+                /*lose++
+                textGepEredmeny.setText("Gép: ${lose}")*/
+                jatekosHp--
+                jatekosHpArray[jatekosHp].setImageResource(R.drawable.heart1)
                 Toast.makeText(this, "Vesztettél", Toast.LENGTH_SHORT).show()
-                textGepEredmeny.setText("Gép: ${lose}")
                 gameEnd()
             }
         }
@@ -84,14 +102,18 @@ class MainActivity : AppCompatActivity() {
             gepRandom = random.nextInt(3)
             if (gepRandom == 0) {
                 imageViewGep.setImageResource(R.drawable.rock)
-                lose++
-                textGepEredmeny.setText("Gép: ${lose}")
+                /*lose++
+                textGepEredmeny.setText("Gép: ${lose}")*/
+                jatekosHp--
+                jatekosHpArray[jatekosHp].setImageResource(R.drawable.heart1)
                 Toast.makeText(this, "Vesztettél", Toast.LENGTH_SHORT).show()
                 gameEnd()
             } else if (gepRandom == 1) {
                 imageViewGep.setImageResource(R.drawable.paper)
-                win++
-                textEmberEredmeny.setText("Ember: ${win}")
+                /*win++
+                textEmberEredmeny.setText("Ember: ${win}")*/
+                gepHp--
+                gepHpArray[gepHp].setImageResource(R.drawable.heart1)
                 Toast.makeText(this, "Nyertél", Toast.LENGTH_SHORT).show()
                 gameEnd()
             } else {
@@ -105,19 +127,31 @@ class MainActivity : AppCompatActivity() {
 
 
     fun init() {
+        gepHp1 = findViewById(R.id.gepHp1)
+        gepHp2 = findViewById(R.id.gepHp2)
+        gepHp3 = findViewById(R.id.gepHp3)
+        jatekosHp1 = findViewById(R.id.jatekosHp1)
+        jatekosHp2 = findViewById(R.id.jatekosHp2)
+        jatekosHp3 = findViewById(R.id.jatekosHp3)
         imageViewEmber = findViewById(R.id.imageViewEmber)
         imageViewGep = findViewById(R.id.imageViewGep)
-        textEmberEredmeny = findViewById(R.id.textEmberEredmeny)
-        textGepEredmeny = findViewById(R.id.textGepEredmeny)
+        /*textEmberEredmeny = findViewById(R.id.textEmberEredmeny)
+        textGepEredmeny = findViewById(R.id.textGepEredmeny)*/
         textViewDontetlen = findViewById(R.id.textViewDontetlen)
         gombKo = findViewById(R.id.gombKo)
         gombPapir = findViewById(R.id.gombPapir)
         gombOllo = findViewById(R.id.gombOllo)
         random = Random;
+        gepHpArray = arrayOf(
+            gepHp1, gepHp2, gepHp3
+        )
+        jatekosHpArray = arrayOf(
+            jatekosHp1, jatekosHp2, jatekosHp3
+        )
     }
 
     fun gameEnd() {
-        if (lose == 3) {
+        if (jatekosHp == 0) {
             var myAlert = AlertDialog.Builder(this)
                 .setTitle("Vereség")
                 .setMessage("Szeretne új játékot játszani?")
@@ -127,7 +161,7 @@ class MainActivity : AppCompatActivity() {
                 }).setNegativeButton("Nem", DialogInterface.OnClickListener { dialog, id ->
                     finishAffinity()
                 }).show()
-        } else if (win == 3) {
+        } else if (gepHp == 0) {
             var myAlert = AlertDialog.Builder(this)
                 .setTitle("Győzelem")
                 .setMessage("Szeretne új játékot játszani?")
@@ -143,12 +177,17 @@ class MainActivity : AppCompatActivity() {
     fun reset() {
         imageViewEmber.setImageResource(R.drawable.rock)
         imageViewGep.setImageResource(R.drawable.rock)
-        win = 0
-        lose = 0
+        gepHp = 3
+        jatekosHp = 3
         draw = 0
-
-        textEmberEredmeny.setText("Ember: ${win}")
-        textGepEredmeny.setText("Gép: ${lose}")
+        for (i in 0..gepHpArray.size-1) {
+            gepHpArray[i].setImageResource(R.drawable.heart2)
+        }
+        for (i in 0..jatekosHpArray.size-1) {
+            jatekosHpArray[i].setImageResource(R.drawable.heart2)
+        }
+        /*textEmberEredmeny.setText("Ember: ${win}")
+        textGepEredmeny.setText("Gép: ${lose}")*/
         textViewDontetlen.setText("Döntetlek száma: ${draw}")
     }
 }
